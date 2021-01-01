0aba3368543ea9d01f2b0295d18e8d32953c6c2a
catsaveearth <tpfbdpf@naver.com>
2021-01-01 21:55:05 +0900

    N과 M(1)

--- BJproblem/15649-1.cpp
+++ BJproblem/15649-1.cpp
@@ -1,28 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int checker[9] = { 0, }; //idx가 바로 숫자에 매칭됨
+int ans[9] = { 0, }; //답을 저장할 배열
+int checker[9] = { 0, }; //방문 체크 배열
+int n, m;
 
-int promising(int n) {
-	if (checker[n]) return 1;
+int promising(int i) {
+	if (!checker[i]) return 1;
 	else return 0;
 }
 
-void bt(int n, int m) {
-	if (n <= m) {
-
+void bt(int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++) {
+			printf("%d ", ans[i]);
+		}
+		printf("\n");
+		return;
 	}
 
-
-
+	for (int i = 1; i <= n; i++) {
+		if (promising(i)) {
+			ans[k] = i;
+			checker[i]++;
+			bt(k + 1);
+			checker[i]--;
+		}
+	}
 }
 
-
-
 int main(void) {
-	int n, m;
 	scanf("%d %d", &n, &m);
-
-
-
+	bt(0);
 }
